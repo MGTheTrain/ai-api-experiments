cfd8f28f871a524e3fd16c6fc03e7be6bb546f24
Marvin Gajek <Marvin.Gajek1@web.de>
2025-02-01 17:02:33 +0100

    reduce chat bot app complexity by removing option of having input prompt content in a file

--- cli/chat_bot_app.py
+++ cli/chat_bot_app.py
@@ -5,27 +5,13 @@ from src.chat_bot import ChatBot
 
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Simple ChatBot")
-    parser.add_argument("--model", type=str, default="gpt-4o", choices=["gpt-4o", "gpt-4o-mini", "o1", "o3-mini"], help="Model to use")
-    parser.add_argument("--prompt", type=str, default="Name me the Millennium Prize Problems and provide me an easy explanation in 2 sentences per problem", help="User prompt")
-    parser.add_argument("--file", type=str, default="inputs/user-content.txt", help="Path to a file containing user content in case message contains to many newlines")
+    parser.add_argument("--model",  type=str, default="gpt-4o", choices=["gpt-4o", "gpt-4o-mini", "o1", "o3-mini"], help="Model to use")
+    parser.add_argument("--prompt", required=True, type=str, default="Name me the Millennium Prize Problems and provide me an easy explanation in 2 sentences per problem", help="User prompt")
     return parser.parse_args()
-
-def load_user_prompt(file_path: str) -> Optional[str]:
-    if not file_path:
-        return None
-    try:
-        with open(file_path, "r") as file:
-            return file.read()
-    except FileNotFoundError:
-        print("User content file not found.")
-        return None
 
 if __name__ == "__main__":
     args = parse_arguments()
     user_prompt = args.prompt
-    if user_prompt is None:
-        user_file='inputs/user-content.txt'
-        user_prompt = load_user_prompt(args.file)
     api_key = os.environ.get("OPENAI_API_KEY")
     if api_key:
         chatbot = ChatBot(api_key, model=args.model)
